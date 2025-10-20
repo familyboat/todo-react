e5b57b755d992a0a9ff311823934c7a705d31a02
familyboat <2015301110129@whu.edu.cn>
2025-10-20 19:05:16 +0800

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
